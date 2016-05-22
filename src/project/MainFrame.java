cf50f38a7e4365ccb15b7171a7b9be7f49a0c159
Hongseok <jk4986@naver.com>
2016-05-22 21:01:34 +0900

    1.0.0 version

---
+++ src/project/MainFrame.java
@@ -0,0 +1,21 @@
+package project;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+	public static void main(String[] args) {
+		MainFrame frame = new MainFrame();
+		frame.setVisible(true);
+		
+	}
+	
+	public MainFrame()
+	{
+		super("Mafia Game");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(1000,600);
+		setVisible(true);
+	}
+	
+}
