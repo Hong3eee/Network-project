d2dfb931582b622b9ad9b378aabfaba00dbf5274
Hongseok <jk4986@naver.com>
2016-05-23 20:49:29 +0900

    version 1.0.1

--- src/project/MainFrame.java
+++ src/project/MainFrame.java
@@ -1,6 +1,5 @@
 package project;
 
-<<<<<<< HEAD
 import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Color;
@@ -176,26 +175,4 @@ public class MainFrame extends JFrame {
 		add(chatting_scroll);
 		
 	}
-
-=======
-import javax.swing.JFrame;
-
-public class MainFrame extends JFrame {
-
-	public static void main(String[] args) {
-		MainFrame frame = new MainFrame();
-		frame.setVisible(true);
-		
-		
-	}
-	
-	public MainFrame()
-	{
-		super("Mafia Game");
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(1000,600);
-		setVisible(true);
-	}
-	
->>>>>>> branch 'master' of https://github.com/Hong3eee/Network-project
 }
